6f05bd9efa69c01144863dd18703dd8cd26f9694
JeremyCaney <jeremy.caney@ignia.com>
2020-12-09 13:29:58 -0800

    Establish a new `AssemblyInfo` for `OnTopic.Data.Transfer`
    These used to be created by default with new projects. They no longer are since most properties have been moved to the `csproj` file. There are some properties that are still useful to set on the assembly, however. Most notably, this sets the library to CLS compliant, which will allow downstream subscribers to trust that it adheres to CLS conventions. While I was at it, I made any internal members visible to the test assembly, and explicitly set the GUID from the solution (`.sln`) file.

---
+++ OnTopic.Data.Transfer/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+/*==============================================================================================================================
+| DEFINE ASSEMBLY ATTRIBUTES
+>===============================================================================================================================
+| Declare and define attributes used in the compiling of the finished assembly.
+\-----------------------------------------------------------------------------------------------------------------------------*/
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("OnTopic.Data.Transfer.Tests")]
+[assembly: GuidAttribute("4F221895-703A-4C49-9E33-7C7394743DDE")]
